1a41815b0f08643f6763f440f51dca55ebb2e4c0
pateldhruvi17 <dhruvipat2004@gmail.com>
2025-06-20 12:06:34 +0530

    Initial commit

---
+++ backend/Mission/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using Mission.Entities;
+using Mission.Entities.Models;
+using Mission.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController(ILoginService loginService, IWebHostEnvironment hostingEnvironment) : ControllerBase
+    {
+        private readonly ILoginService _loginService = loginService;
+        private readonly IWebHostEnvironment _hostingEnvironment = hostingEnvironment;
+        ResponseResult result = new ResponseResult();
+
+        [HttpPost]
+        [Route("LoginUser")]
+        public ResponseResult LoginUser(LoginUserRequestModel model)
+        {
+            try
+            {
+                result.Data = _loginService.LoginUser(model);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<ActionResult> Register(RegisterUserRequestModel model)
+        {
+            try
+            {
+                var res = await _loginService.RegisterUser(model);
+                return Ok(new ResponseResult() { Data = "User Added !", Result = ResponseStatus.Success, Message = "" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseResult() { Data = null, Result = ResponseStatus.Error, Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("LoginUserDetailById/{id}")]
+        public ResponseResult LoginUserDetailById(int id)
+        {
+            try
+            {
+                result.Data = _loginService.LoginUserDetailById(id);
+                result.Result = ResponseStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                result.Result = ResponseStatus.Error;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+
+    }
+}
